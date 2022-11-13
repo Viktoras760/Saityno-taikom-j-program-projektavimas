820f8ce3e98c09ac35647c691b3b0179f3927f19
Viktoras760 <35394736+Viktoras760@users.noreply.github.com>
2022-11-13 18:30:07 +0200

    test

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Schema::defaultStringLength(150);
+        Schema::defaultStringLength(120);
     }
 
     /**
